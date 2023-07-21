c10852d3d996509f67a43acc92c357645a19e9c5
klmlboom1013 <klmlboom1013@gmail.com>
2023-07-21 14:48:34 +0900

    Security Configuration 추가

---
+++ src/main/java/com/wpay/common/global/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.wpay.common.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * Spring Security 설정
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 접근 권한 제어 예외 URI 등록
+        http.antMatcher("/*");
+        return http.build();
+    }
+}
