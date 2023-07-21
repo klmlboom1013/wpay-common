32bac8397c0f0a0cc80aae7a27c67d5ffd628837
klmlboom1013 <klmlboom1013@gmail.com>
2023-07-22 01:57:53 +0900

    LoggingFilter 수정

--- src/main/java/com/wpay/common/global/filter/LoggingFilter.java
+++ src/main/java/com/wpay/common/global/filter/LoggingFilter.java
@@ -8,7 +8,10 @@ import org.springframework.core.annotation.Order;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Log4j2
 @Order(1)
@@ -18,11 +21,15 @@ public class LoggingFilter implements Filter {
 
     private final HttpServletRequest httpServletRequest;
 
+    /** loggingBody 디버깅 여부 (true : 사용 안함) */
+    private static final boolean loggingRequestBodyUnUsedFlag = true;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("==================== {} START ====================", httpServletRequest.getRequestURI());
         this.loggingRequestHeaders();
         this.loggingRequestParams();
+        this.loggingBody(this.httpServletRequest);
         chain.doFilter(request, response);
         log.info("==================== {} END ====================", httpServletRequest.getRequestURI());
     }
@@ -41,5 +48,29 @@ public class LoggingFilter implements Filter {
                 sb.append("    ").append(key).append(" : ").append(this.httpServletRequest.getHeader(key)).append("\n"));
         if(Strings.isNotBlank(sb.toString()))
             log.info("[ Request Parameters ]\n{}",sb.toString());
+    }
+
+    public void loggingBody(HttpServletRequest request) throws IOException {
+
+        if(loggingRequestBodyUnUsedFlag) return;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            }
+        } finally {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+        log.info("requestBody: \n{}", stringBuilder.toString());
     }
 }
