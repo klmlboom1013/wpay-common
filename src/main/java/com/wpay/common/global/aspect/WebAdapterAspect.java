75ce02ca80c5fd417bde85313cce7f8ccedc6a43
klmlboom1013 <klmlboom1013@gmail.com>
2023-08-11 12:30:59 +0900

    롤백

--- src/main/java/com/wpay/common/global/aspect/WebAdapterAspect.java
+++ src/main/java/com/wpay/common/global/aspect/WebAdapterAspect.java
@@ -1,8 +1,5 @@
 package com.wpay.common.global.aspect;
 
-import com.wpay.common.global.dto.BaseCommand;
-import com.wpay.common.global.exception.CustomException;
-import com.wpay.common.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
@@ -10,9 +7,6 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 @Log4j2
@@ -27,30 +21,6 @@ public class WebAdapterAspect extends BaseAspect {
     @Override
     public void before(JoinPoint joinPoint) {
         log.info("[Before] => {}", joinPoint.getSignature().getName());
-
-        List<BaseCommand<?>> baseCommands = new ArrayList<>();
-        Arrays.stream(joinPoint.getArgs()).anyMatch(o -> {
-            /* SelfCrypto 을 상속 받지 않았으면 continue */
-            if ((Boolean.FALSE.equals(o instanceof BaseCommand))) return false;
-            baseCommands.add((BaseCommand<?>)o);
-            return true;
-        });
-
-        log.debug("CommandDto class full name: {}", baseCommands.get(0).getClass().getName());
-
-        if(baseCommands.isEmpty()) {
-            log.error("해당 API의 내부 정책 오류 입니다. BaseCommand를 상속 받은 RequestBody DTO가 존재 하지 않습니다.");
-            throw new CustomException(ErrorCode.HTTP_STATUS_500);
-        }
-        if(baseCommands.get(0).getClass().getName().indexOf("application.port.in.dto") < 1) {
-            log.error("해당 API의 내부 정책 오류 입니다. BaseCommand 구현 DTO 생성 경로 오류 입니다.");
-            throw new CustomException(ErrorCode.HTTP_STATUS_500);
-        }
-        if(baseCommands.get(0).getClass().getSimpleName().indexOf("Command") < 1) {
-            log.error("해당 API의 내부 정책 오류 입니다. BaseCommand 구현 DTO의 이름 마지막에 \"Command\" 문자가 포함 되어야 합니다.");
-            throw new CustomException(ErrorCode.HTTP_STATUS_500);
-        }
-
         this.baseCommandValidateCryptoSelf(joinPoint, request);
     }
 
